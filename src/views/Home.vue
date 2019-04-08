6293f1417bcf9fe190f83732735d863ef361f888
Jack Vaught <johnmvaught@gmail.com>
2019-04-08 10:09:51 -0700

    simple parallax sorta works

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-on:scroll.native="handleScroll">
   <div class="home">
     <div class="image-container">
       <img class="image" alt="" src="../assets/maria_main.jpg">
@@ -12,28 +12,52 @@
 // @ is an alias to /src
 import InstaFeed from '@/components/InstaFeed.vue'
 
+
 export default {
   name: 'home',
   components: {
     InstaFeed
   },
+  methods: {
+    handleScroll () {
+      let el = document.querySelector('.title').style.top = .8 * window.scrollY + 110 + 'px';
+    },
+    debounceScroll (func, limit) {
+      let inThrottle
+      return function() {
+        const args = arguments
+        const context = this
+        if (!inThrottle) {
+          func.apply(context, args)
+          inThrottle = true
+          setTimeout(() => inThrottle = false, limit)
+        }
+      }
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.debounceScroll(this.handleScroll, 10));
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.debounceScroll(this.handleScroll, 10));
+  }
 }
 </script>
 
 <style scoped>
   .image-container {
-    height: calc(100vh - 80px);
+    min-height: calc(100vh);
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: flex-start;
+    align-items: flex-start;
     font-size: 30px;
     position: relative;
-    top: 80px;
+    top: 0px;
+    overflow: hidden;
   }
 
   .image {
     width: 100%;
-    max-height: 100%;
     object-fit: cover;
     /* margin: 0 120px; */
   }
@@ -41,7 +65,7 @@ export default {
   .title {
   position: absolute;
   left: 30px;
-  top: 30px;
+  top: 110px;
   color: white;
   font-size: 100px;
   font-weight: 800;
